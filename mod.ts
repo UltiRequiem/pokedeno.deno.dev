ade82ad5955bbfef57e7b65fd7f3e1af38ac3cef
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-06-11 18:38:17 -0500

    :sparkles: Pokemon

--- mod.ts
+++ mod.ts
@@ -1,3 +1,4 @@
 import { serve } from "https://deno.land/std@0.142.0/http/server.ts";
+import pokemon from "https://esm.sh/pokemon";
 
-serve((req: Request) => new Response("Hello World"));
+serve((_req: Request) => new Response(pokemon.random()));
